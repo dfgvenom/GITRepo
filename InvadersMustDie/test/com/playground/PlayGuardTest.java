3fbb3a5e04d7cb8856b4087a426253ef277882ef
dfgvenom <tim.kloeckner@tu-dortmund.de>
2017-02-11 19:59:48 +0100

    test erstellt und JUnit zum Classpath hinzugefügt

---
+++ InvadersMustDie/test/com/playground/PlayGuardTest.java
@@ -0,0 +1,14 @@
+package com.playground;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayGuardTest {
+
+	@Test
+	public void testName() throws Exception {
+		assertTrue(true);
+	}
+	
+}
